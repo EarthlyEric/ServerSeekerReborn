b72b0de16e43579cf7c044ee32de22c1a3c3c5c9
EarthlyEric <earthlyeric6@gmail.com>
2025-01-03 20:45:15 +0800

    add massScannerOutputParser

---
+++ lib/utils.py
@@ -0,0 +1,19 @@
+import mcstatus
+
+class massScannerOutputParser:
+    @staticmethod
+    def porcess(file:str):
+        IPs = []
+        try:
+            with open(file, 'r') as fileHandler:
+                listOfLines = fileHandler.readlines()
+            fileHandler.close()
+        except:
+            raise Exception("Error: File not found")
+        
+        for line in listOfLines:
+            if line.strip()[0] != "#":
+                IPs.append(line.strip().split(' ',4)[3])
+                
+        return IPs
+    
