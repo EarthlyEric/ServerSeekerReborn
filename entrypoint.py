387a26ef5dc29ec561d8626258c214604f10efef
EarthlyEric <earthlyeric6@gmail.com>
2025-01-04 23:43:18 +0800

    initialize staging

--- entrypoint.py
+++ entrypoint.py
@@ -1,4 +1,18 @@
-from lib import config
+import os
+import pyfiglet
+from multiprocessing import Process, set_start_method
 
-conf = config.Config()
-
+def initializeTask():
+    def beat():
+        os.system("celery -A tasks beat --loglevel=info")
+        
+    beatProcess = Process(target=beat)
+    beatProcess.start()
+    beatProcess.join()
+    
+if __name__ == "__main__":
+    font = pyfiglet.Figlet(direction="auto",width=60)
+    print(font.renderText("SSReborn"))
+    if os.name == "nt":
+        set_start_method("spawn")
+    initializeTask()
