ea32e9647105e14bd967437c334eeed179c635bd
EarthlyEric <earthlyeric6@gmail.com>
2025-01-06 23:12:27 +0800

    new class

--- lib/database.py
+++ lib/database.py
@@ -1,4 +1,2 @@
-import psycopg2-binary
-from lib.config import Config
+import psycopg
 
-conf
---
+++ lib/statuspinger.py
@@ -0,0 +1,6 @@
+import mcstatus
+
+class muiltThreadStatusPinger:
+    def __init__(self, server_list: list):
+        self.ip_list = server_list
+
